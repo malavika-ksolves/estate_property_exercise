b6bb77f0988c33024baaa9db8d9e2a060cf2ce9d
malavika-ksolves <malavika.ar@ksolves.com>
2022-06-09 13:51:40 +0530

    Created new modules

---
+++ odooschool/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OdooSchool',
+    'depends': ['base_setup'],
+    'data': [
+        'security/ir.model.access.csv'
+        ],
+    'application': True,
+
+}
---
+++ odooschool/models/student.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class Student(models.Model):
+
+    _name = 'student.odoo.school'
+    _description = 'Student Details'
+
+
+
